fc40e99b30895998654a65bc02ac71c48654eeb4
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2021-08-02 14:41:55 -0500

    added html template

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ window.addEventListener("load", function() {
 
 this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
     response.json().then(function(json){
+        let astroData = "";
+        for(let i = 0; i < json.length; i++) {
+            let astronaut = json[i];
+           
+         astroData +=  ` <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+           <ul>
+           <li>Hours in space: ${astronaut.hoursInSpace}</li>
+           <li> Active: ${astronaut.active}</li>
+           <li> Skills: ${astronaut.skills.join(", ")}</li>
+
+         </ul>
+    ` 
+        }
         const astroDiv = document.getElementById("container");
-        astroDiv.innerHTML = `
-            //I'll put api info here
+        astroDiv.innerHTML = astroData;
+            
         
-        `
         
     });
 
