fd193dd6ac001ab77138d60b99499cada56ff057
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2021-08-02 13:09:52 -0500

    getting started

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+
+this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+    response.json().then(function(json){
+        
+    });
+
+})
+
+
+
+});
