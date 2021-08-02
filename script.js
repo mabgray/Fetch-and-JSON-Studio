3158f0dc76deae824e6bc3b2e2036f3540424efe
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2021-08-02 13:13:51 -0500

    more work on fetch

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ window.addEventListener("load", function() {
 
 this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
     response.json().then(function(json){
+        const astroDiv = document.getElementById("container");
+        astroDiv.innerHTML = `
+            //I'll put api info here
+        
+        `
         
     });
 
