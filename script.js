b8c3d62bbc9e606453385b89c93a645f702e5600
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2021-08-03 20:13:10 -0500

    worked on bonus mission

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 window.addEventListener("load", function() {
 
 this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
-    response.json().then(function(json){
-        let astroData = "";
+    response.json().then(function(json)
+    { //json() formats response into a js object. 
+        let astroData = "";//make a string and put the whole string in 
         for(let i = 0; i < json.length; i++) {
             let astronaut = json[i];
            
+            
+
          astroData +=  ` <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
            <ul>
            <li>Hours in space: ${astronaut.hoursInSpace}</li>
            <li> Active: ${astronaut.active}</li>
            <li> Skills: ${astronaut.skills.join(", ")}</li>
+         <img src="${astronaut.picture}" alt="image of astronaut"/>
 
          </ul>
+         
     ` 
+
+    
+    
         }
         const astroDiv = document.getElementById("container");
         astroDiv.innerHTML = astroData;
